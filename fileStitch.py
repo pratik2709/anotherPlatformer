5aea20803a3a6420b1c36981f4c756eb271b506a
pratiksaxena <pratik2709@gmail.com>
2018-12-08 17:24:20 +0530

    created a valid pico8 file

--- fileStitch.py
+++ fileStitch.py
@@ -10,7 +10,15 @@ f.write(header.read())
 
 header.close()
 
-# for item in items:
-#     f.write(item.read())
 
+for item in items:
+    if(item != "headers.p8" and item != "gfx.p8"):
+        f.write("\n\n -- **************" + item + "****************\n\n")
+        ff = open("src/" + item, "r")
+        f.write(ff.read())
+        ff.close()
+
+f.write("\n\n -- **************" + "gfx.p8" + "****************\n\n")
+footer= open("src/gfx.p8", 'r')
+f.write(footer.read())
 f.close()
